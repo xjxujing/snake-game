c14db472b1a5b72b50aa2362e2bde2f031cc664a
xjxujing <xujing_xj@foxmail.com>
2021-05-28 11:56:03 +0800

    删掉部分 hooks

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Game from './Game';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Game />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
